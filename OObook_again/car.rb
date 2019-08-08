847ba0ce5246a471de2761f92284eee1a8d25e7f
LR <lr177245@gmail.com>
2019-08-08 13:22:22 -0700

    reading OO book again

---
+++ OObook_again/car.rb
@@ -0,0 +1,49 @@
+class MyCar
+  MAX_SPEED = 100
+  BRAKE_INCREMENT = 10
+
+  attr_accessor :color
+  attr_reader :model, :year
+
+  def self.mpg(gallons_used, miles_traveled)
+    (miles_traveled / gallons_used.to_f).round(1)
+  end
+  
+  def initialize(year, color, model)
+    @year  = year
+    @color = color
+    @model = model
+    @current_speed = 0
+    @car_on = false
+  end
+
+  def speed_up
+    @current_speed += 5 unless @current_speed >= MAX_SPEED
+  end
+
+  def brake
+    if @current_speed < BRAKE_INCREMENT
+      @current_speed = 0
+    else
+      @current_speed -= 10
+    end
+  end
+
+  def turn_off
+    @car_on = false
+  end
+
+  def turn_on
+    @car_on = true
+  end
+
+  def spray_paint(color)
+    self.color = color
+  end
+
+  def to_s
+    "Year: #{year}"  \
+    "Model #{model}" \
+    "Color #{color}"
+  end
+end
